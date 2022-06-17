ce49d2ae9664a56a2d7ce71c78a9791bf9dbdb8f
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-17 17:35:28 +0300

    setup MVC

---
+++ LogInApp/Model/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  LogInApp
+//
+//  Created by Nikolai Maksimov on 17.06.2022.
+//
+
+import Foundation
+
+struct User {
+    
+    let user: String
+    let password: String
+    let person: Person
+    
+    static func getUser() -> User {
+        User(user: "User", password: "Password", person: Person.getPerson())
+    }
+}
+
+struct Person {
+    
+    let name: String
+    let surName: String
+    var fullName: String {
+        "\(name) \(surName)"
+    }
+    
+    static func getPerson() -> Person {
+        Person(name: "John", surName: "Smith")
+    }
+}
